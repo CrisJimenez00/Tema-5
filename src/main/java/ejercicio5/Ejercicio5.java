3afb677f214743ed0711b824c446700b732bc9d1
cristinaj1 <cristinacee305@gmail.com>
2022-01-24 19:53:06 +0100

    Ejercicio 5 hecho completamente sin corregir

--- src/main/java/ejercicio5/Ejercicio5.java
+++ src/main/java/ejercicio5/Ejercicio5.java
@@ -22,8 +22,86 @@ public class Ejercicio5 {
         for (int i = 0; i < array.length; i++) {
             System.out.println("Introduzca el valor que quiera que se encuentre en la posición " + i);
             int numero = teclado.nextInt();
-            array[i]= numero;
+            array[i] = numero;
         }
+    }
+
+    private static int contarPositivos(int[] array) {
+        int contador = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    private static int contarNegativos(int[] array) {
+        int contador = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    private static int contarCeros(int[] array) {
+        int contador = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    private static void mostrarArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " - ");
+        }
+
+    }
+
+    private static void mediaPositivo(int[] array) {
+        double media = 0;
+        int contador = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
+                contador++;
+            }
+            media += array[i];
+        }
+        media = media / contador;
+        System.out.println("La media de los números positivos es de: " + media);
+    }
+
+    private static void mediaNegativo(int[] array) {
+        double media = 0;
+        int contador = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                contador++;
+            }
+            media += array[i];
+        }
+        media = media / contador;
+        System.out.println("La media de los números negativos es de: " + media);
+    }
+
+    public static void main(String[] args) {
+        leerValores();
+        mostrarArray(array);
+        System.out.println("Contar positivos " + contarPositivos(array));
+        System.out.println("Contar negativos " + contarNegativos(array));
+        System.out.println("Contar ceros " + contarCeros(array));
+        mediaPositivo(array);
+        mediaNegativo(array);
     }
 
 }
