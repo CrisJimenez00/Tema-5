9baaee46e29356d9e6908c5759097095f56adcd8
cristinaj1 <cristinacee305@gmail.com>
2022-02-16 21:13:22 +0100

    Ejercicio avió comenzad

--- src/main/java/ejercicio19/Avion.java
+++ src/main/java/ejercicio19/Avion.java
@@ -11,7 +11,25 @@ package ejercicio19;
 public class Avion {
 
     public static void main(String[] args) {
+        //mirar si se puede con boolean o con 1 y 2
+        boolean[][] asientos = new boolean[25][4];
+
+        //Primero ponemos todos los asientos en false(libres)
+        for (int i = 0; i < 25; i++) {
+            for (int j = 0; j < 4; j++) {
+                asientos[i][j] = false;
+            }
+        }
+        //for dentro de un for para mostrarlo por pantalla
+        for (int i = 0; i < 25; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(asientos[i][j]);
+                System.out.print("\t");
+            }
+            System.out.println();
+        }
         
+
     }
-    
+
 }
