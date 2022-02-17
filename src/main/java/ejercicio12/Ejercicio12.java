8718fef28b6c6dafcab0bc8fdaed9678dc36a140
CrisJimenez00 <cristina.del.carmen.jimenez.1@gmail.com>
2022-02-17 12:25:11 +0100

    Ejercicio 12 subido y acabado

---
+++ src/main/java/ejercicio12/Ejercicio12.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio12;
+
+import java.util.Random;
+
+/**
+ *
+ * @author cristina
+ */
+public class Ejercicio12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[][] matriz = new int[2][3];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = random.nextInt(10) + 1;
+            }
+
+        }
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j]);
+                System.out.print("\t");
+            }
+            System.out.println();
+        }
+    }
+
+}
